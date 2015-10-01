e31a02a4edeaa325e87a6f51ab4b4af645679772
Adam Worrall <abw@abw-macbookpro3.roam.corp.google.com>
2015-09-30 21:37:52 -0700

    Initial checkin

---
+++ adsb.go
@@ -0,0 +1,56 @@
+package adsb
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/skypies/geo"
+)
+
+type IcaoId string
+
+// http://woodair.net/SBS/Article/Barebones42_Socket_Data.htm
+// https://github.com/MalcolmRobb/dump1090/blob/master/mode_s.c#L834
+type Msg struct {
+	// This set of data is basically the SBS1 format.
+
+	Type string // = 0 // type	 (MSG, STA, ID, AIR, SEL or CLK)
+	SubType int64 // = 1 // Type	 MSG sub types 1 to 8. Not used by other message types.
+	// Session = 2 // ID	 Database Session record number
+	// AircraftID = 3 //	 Database Aircraft record number
+	Icao24 IcaoId // [represent as []byte ?]  = 4 //	 Aircraft Mode S hexadecimal code
+	GeneratedTimestampUTC time.Time
+	LoggedTimestampUTC    time.Time
+	//DateGen = 6 // message generated	  As it says
+	//TimeGen = 7 // message generated	  As it says
+	//DateLog = 8 // message logged	  As it says
+	//TimeLog = 9 // message logged	  As it says
+	Callsign string // = 10 //	 An eight digit flight ID - can be flight number or registration
+	Altitude int64 // = 11 //	 Mode C altitude. Height relative to 1013.2mb (Flight Level).
+	GroundSpeed int64 // = 12 //	 Speed over ground (not indicated airspeed)
+	Track int64 // = 13 //	 Track of aircraft (not heading). Derived from the velocity E/W, N/S
+
+	Position geo.Latlong
+	//Latitude float64 // = 14 //	 North and East positive. South and West negative.
+	//Longitude float64 // = 15 //	 North and East positive. South and West negative.
+
+	VerticalRate int64 // = 16 //	 64ft resolution
+	Squawk string // = 17 //	 Assigned Mode A squawk code.
+	AlertSquawkChange bool // = 18 // (Squawk change)	 Flag to indicate squawk has changed.
+	Emergency bool // = 19 //	 Flag to indicate emergency code has been set
+	SPI bool // = 20 // (Ident)	 Flag to indicate transponder Ident has been activated.
+	IsOnGround bool // = 21 //	 Flag to indicate ground squat switch is active
+
+	// Flags filled during parsing, interrogated by methods below
+	hasPosition bool
+}
+
+func (m Msg)HasPosition() bool { return m.hasPosition }
+
+func (m Msg)String() string {
+	s := fmt.Sprintf("%s%d : %s", m.Type, m.SubType, m.Icao24)
+	if m.HasPosition() {
+		s += fmt.Sprintf(" %s", m.Position)
+	}
+	return s
+}
---
+++ sbs1.go
@@ -0,0 +1,105 @@
+package adsb
+
+import (
+	"encoding/csv"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/skypies/geo"
+)
+// http://woodair.net/SBS/Article/Barebones42_Socket_Data.htm
+const (
+	SBS1Message = 0 // type	 (MSG, STA, ID, AIR, SEL or CLK)
+	SBS1Transmission = 1 // Type	 MSG sub types 1 to 8. Not used by other message types.
+	SBS1Session = 2 // ID	 Database Session record number
+	SBS1AircraftID = 3 //	 Database Aircraft record number
+	SBS1Icao24 = 4 //	 Aircraft Mode S hexadecimal code
+	SBS1FlightID = 5 //	 Database Flight record number
+	SBS1DateGen = 6 // message generated	  As it says
+	SBS1TimeGen = 7 // message generated	  As it says
+	SBS1DateLog = 8 // message logged	  As it says
+	SBS1TimeLog = 9 // message logged	  As it says
+	SBS1Callsign = 10 //	 An eight digit flight ID - can be flight number or registration (or even nothing).
+	SBS1Altitude= 11 //	 Mode C altitude. Height relative to 1013.2mb (Flight Level). Not height AMSL..
+	SBS1GroundSpeed = 12 //	 Speed over ground (not indicated airspeed)
+	SBS1Track = 13 //	 Track of aircraft (not heading). Derived from the velocity E/W and velocity N/S
+	SBS1Latitude = 14 //	 North and East positive. South and West negative.
+	SBS1Longitude = 15 //	 North and East positive. South and West negative.
+	SBS1VerticalRate = 16 //	 64ft resolution
+	SBS1Squawk = 17 //	 Assigned Mode A squawk code.
+	SBS1AlertSquawkChange = 18 // (Squawk change)	 Flag to indicate squawk has changed.
+	SBS1Emergency = 19 //	 Flag to indicate emergency code has been set
+	SBS1SPI = 20 // (Ident)	 Flag to indicate transponder Ident has been activated.
+	SBS1IsOnGround = 21 //	 Flag to indicate ground squat switch is active
+)
+
+
+func toTime(d,t string) (time.Time, error) {
+	if t,err := time.Parse("2006/01/02 15:04:05.999999999", d+" "+t); err != nil {
+		return time.Now(),err
+	} else {
+		// TODO(abw): TIMEZONES
+		return t,nil
+	}
+}
+
+
+func (m *Msg)FromSBS1(s string) error {
+	ioReader := strings.NewReader(s)
+	csvReader := csv.NewReader(ioReader)
+	if r,err := csvReader.Read(); err != nil {
+		return err
+	} else {
+		// Dear god. If any block of code ever needed try/catch ...
+		m.Type = r[SBS1Message]
+		if i,err := strconv.ParseInt(r[SBS1Transmission], 10, 64); err != nil {
+			return err
+		} else {
+			m.SubType = i
+		}
+		m.Icao24 = IcaoId(r[SBS1Icao24])
+
+		
+		if t,err := toTime(r[SBS1DateGen], r[SBS1TimeGen]); err != nil {
+			return err
+		} else {
+			m.GeneratedTimestampUTC = t
+		}
+		if t,err := toTime(r[SBS1DateLog], r[SBS1TimeLog]); err != nil {
+			return err
+		} else {
+			m.LoggedTimestampUTC = t
+		}
+
+		m.Callsign = r[SBS1Callsign]
+
+		if (r[SBS1Altitude] != "") {
+			if i,err := strconv.ParseInt(r[SBS1Altitude], 10, 64); err != nil {
+				return err
+			} else {
+				m.Altitude = i
+			}
+		}
+		if (r[SBS1GroundSpeed] != "") {
+			if i,err := strconv.ParseInt(r[SBS1GroundSpeed], 10, 64); err != nil {
+				return err
+			} else {
+				m.GroundSpeed = i
+			}
+		}
+		
+		// Shoud prob decide this based on message type.
+		if (r[SBS1Latitude] != "" && r[SBS1Longitude] != "") {
+			if lat,err := strconv.ParseFloat(r[SBS1Latitude], 64); err != nil {
+				return err
+			} else if long,err := strconv.ParseFloat(r[SBS1Longitude], 64); err != nil {
+				return err
+			} else {
+				m.Position = geo.Latlong{lat, long}
+				m.hasPosition = true
+			}
+		}
+	}
+	return nil
+}
