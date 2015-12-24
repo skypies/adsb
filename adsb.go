955fed642d42b7642837878d1d475a7c7a34efdc
Adam Worrall <adam@worrall.cc>
2015-12-24 10:12:02 -0800

    Take a different take with message signatures. It turns out that different receivers will end up with different cached state for a given IcaoId, and so will emit slightly different messages for the same location. So we just use position and IcaoId to dedupe messages across receivers.

--- adsb.go
+++ adsb.go
@@ -9,19 +9,6 @@ import (
 
 type IcaoId string
 
-// These fields are pulled out into their own object; this can be used as a hash key
-type MsgContent struct {
-	Icao24 IcaoId //  = 4 //	 Aircraft Mode S hexadecimal code
-	Callsign string // = 10 //	 An eight digit flight ID - can be flight number or registration
-	Altitude int64 // = 11 //	 Mode C altitude. Height relative to 1013.2mb (Flight Level).
-	GroundSpeed int64 // = 12 //	 Speed over ground (not indicated airspeed)
-	Track int64 // = 13 //	 Track of aircraft (not heading). Derived from the velocity E/W, N/S
-	Position geo.Latlong
-	VerticalRate int64 // = 16 //	 64ft resolution
-	Squawk string // = 17 //	 Assigned Mode A squawk code.
-}
-
-
 // http://woodair.net/SBS/Article/Barebones42_Socket_Data.htm
 // https://github.com/MalcolmRobb/dump1090/blob/master/mode_s.c#L834
 //
@@ -29,31 +16,29 @@ type MsgContent struct {
 //
 type Msg struct {
 	// This set of data is basically the SBS1 format, not the ADS-B format.
-
-	MsgContent  // Embed all the hashable 'content' fields that an be used to dedupe
 	
 	Type string // = 0 // type	 (MSG, STA, ID, AIR, SEL or CLK). We ignore all but MSG.
 	SubType int64 // = 1 // Type	 MSG sub types 1 to 8. Not used by other message types.
 	// Session = 2 // ID	 Database Session record number
 	// AircraftID = 3 //	 Database Aircraft record number
-	// -- in MsgContent -- Icao24 IcaoId //  = 4 //	 Aircraft Mode S hexadecimal code
+	Icao24 IcaoId //  = 4 //	 Aircraft Mode S hexadecimal code
 	GeneratedTimestampUTC time.Time
 	LoggedTimestampUTC    time.Time
 	//DateGen = 6 // message generated	  As it says
 	//TimeGen = 7 // message generated	  As it says
 	//DateLog = 8 // message logged	  As it says
 	//TimeLog = 9 // message logged	  As it says
-	// -- in MsgContent -- Callsign string // = 10 //	 An eight digit flight ID - can be flight number or registration
-	// -- in MsgContent -- Altitude int64 // = 11 //	 Mode C altitude. Height relative to 1013.2mb (Flight Level).
-	// -- in MsgContent -- GroundSpeed int64 // = 12 //	 Speed over ground (not indicated airspeed)
-	// -- in MsgContent -- Track int64 // = 13 //	 Track of aircraft (not heading). Derived from the velocity E/W, N/S
+	Callsign string // = 10 //	 An eight digit flight ID - can be flight number or registration
+	Altitude int64 // = 11 //	 Mode C altitude. Height relative to 1013.2mb (Flight Level).
+	GroundSpeed int64 // = 12 //	 Speed over ground (not indicated airspeed)
+	Track int64 // = 13 //	 Track of aircraft (not heading). Derived from the velocity E/W, N/S
 
-	// -- in MsgContent -- Position geo.Latlong
+	Position geo.Latlong
 	//Latitude float64 // = 14 //	 North and East positive. South and West negative.
 	//Longitude float64 // = 15 //	 North and East positive. South and West negative.
 
-	// -- in MsgContent -- VerticalRate int64 // = 16 //	 64ft resolution
-	// -- in MsgContent -- Squawk string // = 17 //	 Assigned Mode A squawk code.
+	VerticalRate int64 // = 16 //	 64ft resolution
+	Squawk string // = 17 //	 Assigned Mode A squawk code.
 	AlertSquawkChange bool // = 18 // (Squawk change)	 Flag to indicate squawk has changed.
 	Emergency bool // = 19 //	 Flag to indicate emergency code has been set
 	SPI bool // = 20 // (Ident)	 Flag to indicate transponder Ident has been activated.
@@ -73,6 +58,21 @@ func (m Msg)String() string {
 	return s
 }
 
+// Signature is a subset of a composite ADSB message that can be considered
+// to identify the content of the message; if two messages have equivalent
+// Signatures, then we can consider them to be identical / duplicates.
+type Signature struct {
+	Pos geo.Latlong
+	Icao24 IcaoId
+}
+func (m *CompositeMsg)GetSignature() Signature {
+	return Signature{
+		Pos: m.Position,
+		Icao24: m.Icao24,
+	}
+}
+
+
 // CompositeMsg has the same data fields as a Msg, but contains data from multiple Msgs. This is
 // because ADS-B messages are normally emitted with only some fields filled; to get altitude, speed
 // location *and* callsign, you need to combine ~3 messages. We use a distinct type for these, to be
@@ -84,9 +84,16 @@ type CompositeMsg struct {
 }
 
 func (cm CompositeMsg)String() string {
-	return fmt.Sprintf("%s%d+ : %s[%7.7s] %3dk, %5df/m, %3ddeg, %s @ %s (%s)",
+	pos := fmt.Sprintf(" (%.7f,%.7f)", cm.Position.Lat, cm.Position.Long)
+	return fmt.Sprintf("%s%d+ : %s[%7.7s] %5df, %3dk, %5df/m, %3ddeg, %s @ %s (%s)",
 		cm.Type, cm.SubType, cm.Icao24, cm.Callsign,
-		cm.GroundSpeed, cm.VerticalRate, cm.Track, cm.Position, cm.GeneratedTimestampUTC,
+		cm.Altitude, cm.GroundSpeed, cm.VerticalRate, cm.Track, pos, cm.GeneratedTimestampUTC,
 		cm.ReceiverName)
 }
 
+type CompositeMsgPtrByTimeAsc []*CompositeMsg
+func (a CompositeMsgPtrByTimeAsc) Len() int          { return len(a) }
+func (a CompositeMsgPtrByTimeAsc) Swap(i,j int)      { a[i],a[j] = a[j],a[i] }
+func (a CompositeMsgPtrByTimeAsc) Less(i,j int) bool {
+	return a[j].GeneratedTimestampUTC.After(a[i].GeneratedTimestampUTC)
+}
