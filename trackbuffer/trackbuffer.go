84591b12fb25bb328e6233b4851bf55a6930f3c1
Adam Worrall <adam@worrall.cc>
2017-02-03 08:24:27 -0800

    Make auto-flushing more tolerant of processing stale messages.

--- trackbuffer/trackbuffer.go
+++ trackbuffer/trackbuffer.go
@@ -16,12 +16,14 @@ type Track struct {
 type TrackBuffer struct {
 	MaxAge      time.Duration // Flush any track with data older than this
 	Tracks      map[adsb.IcaoId]*Track
+	lastFlush   time.Time
 }
 
 func NewTrackBuffer() *TrackBuffer {
 	tb := TrackBuffer{
 		MaxAge: time.Second*30,
 		Tracks: make(map[adsb.IcaoId]*Track),
+		lastFlush: time.Now(),
 	}
 	return &tb
 }
@@ -57,6 +59,16 @@ func (tb *TrackBuffer)AddMessage(m *adsb.CompositeMsg) {
 
 // Flushing should be automatic and internal, not explicit like this.
 func (tb *TrackBuffer)Flush(flushChan chan<- []*adsb.CompositeMsg) {
+	// When we get late or out-of-order delivery, the timestamps in the messages will be so
+	// old that they will trigger immediate flushing every time. This causes so many DB writes
+	// that the system can't keep up, so we never get back to useful buffering. Put a mild rate
+	// limiter in here.
+	if time.Since(tb.lastFlush) < time.Second {
+		return
+	} else {
+		tb.lastFlush = time.Now()
+	}
+
 	toRemove := []adsb.IcaoId{}
 	
 	for id,_ := range tb.Tracks {
